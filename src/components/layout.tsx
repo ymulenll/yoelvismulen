d123cdfb01dc19647c27a507235b25cf1a8a40cc
Yoelvis Mulen <ymulenll@gmail.com>
2021-08-01 22:44:15 -0300

    basic configuration with tailwindcss

---
+++ src/components/layout.tsx
@@ -0,0 +1,7 @@
+const siteTitle = 'Yoelvis Mulen { code }'
+
+type Props = { children: React.ReactNode }
+
+export default function Layout({ children }: Props) {
+  return <div>{children}</div>
+}
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+// https://tailwindcss.com/docs/customizing-colors
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      red: colors.red,
+      yellow: colors.amber,
+      green: colors.emerald,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.violet,
+      pink: colors.pink,
+
+      orange: colors.orange,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
