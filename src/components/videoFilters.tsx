305b5408eda1a705e9939bebde0a53c3702c1204
Yoelvis Mulen <ymulenll@gmail.com>
2021-08-07 12:37:40 -0300

    display different number of tags to different devices

--- src/components/videoFilters.tsx
+++ src/components/videoFilters.tsx
@@ -1,3 +1,6 @@
+import { useRouter } from 'next/dist/client/router'
+import { BREAKPOINTS } from '../constants'
+import { useWindowSize } from '../hooks/useWindowSize'
 import Tag from './tag'
 
 interface Props {
@@ -5,9 +8,32 @@ interface Props {
 }
 
 export const VideoFilters = ({ tags }: Props) => {
+  const windowSize = useWindowSize()
+  const { query } = useRouter()
+  const tagFilter = query?.tags as string
+
+  const tagEntries = Object.entries(tags)
+
+  const tagDisplayNumber = (windowSize.width ?? 0) >= BREAKPOINTS.md ? 7 : 5
+
+  const filterSelectedEntryIfNotVisible = (
+    [tag]: [string, number],
+    index: number
+  ) => tag === tagFilter && index > tagDisplayNumber
+
+  const selectedTagEntry = tagEntries.filter(filterSelectedEntryIfNotVisible)
+  const restOfEntries = tagEntries.filter(
+    (tagEntry, index) => !filterSelectedEntryIfNotVisible(tagEntry, index)
+  )
+
+  const entriesDisplay = [...selectedTagEntry, ...restOfEntries].slice(
+    0,
+    tagDisplayNumber
+  )
+
   return (
-    <div className="line-clamp-1">
-      {Object.entries(tags).map(([tag, quantity]) => (
+    <div className="ml-2 text-center" role="tablist">
+      {entriesDisplay.map(([tag, quantity]) => (
         <Tag key={tag} quantity={quantity}>
           {tag}
         </Tag>
