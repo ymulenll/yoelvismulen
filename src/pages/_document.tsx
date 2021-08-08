79cb172a61c05be85a73c6abbd74f12a2cfe3bfa
Yoelvis Mulen <ymulenll@gmail.com>
2021-08-08 17:42:00 -0300

    insert google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,18 @@ class MyDocument extends Document {
     return (
       <Html lang="es">
         <Head>
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NCPFZXF');`,
+            }}
+          ></script>
+          {/* End Google Tag Manager */}
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
@@ -28,6 +40,14 @@ class MyDocument extends Document {
           />
         </Head>
         <body className="bg-gray-50">
+          {/* Google Tag Manager (noscript) */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NCPFZXF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
+          {/* End Google Tag Manager (noscript) */}
           <Main />
           <NextScript />
         </body>
