83a096bddd0a4bcb09a84fdd636762a0d7a45047
Yoelvis Mulen <ymulenll@gmail.com>
2022-11-01 16:52:49 -0400

    add initial api handler

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -7,7 +7,7 @@ type Data = {
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(JSON.stringify(req))
 }
